357eaeeff1f089939c7c107e7744cd011e1cc75e
Anton Bodiak <abodiak@prometheusgroup.com>
2020-12-18 01:15:27 +0200

    Create 'MainForm.GetSelectedRowIndex'

--- MainForm.cs
+++ MainForm.cs
@@ -30,11 +30,7 @@ namespace SPZLab5Var1
 
         private void teacherEditButton_Click(object sender, EventArgs e)
         {
-            var selectedRowIndex = teachersDataGridView.SelectedRows.Count == 1
-                ? teachersDataGridView.SelectedRows[0].Index
-                : teachersDataGridView.SelectedCells.Count == 1
-                ? teachersDataGridView.SelectedCells[0].RowIndex
-                : (int?)null;
+            var selectedRowIndex = GetSelectedRowIndex();
             if (selectedRowIndex == null)
             {
                 return;
@@ -50,6 +46,12 @@ namespace SPZLab5Var1
                 }
             ).Show();
         }
+
+        private int? GetSelectedRowIndex() => teachersDataGridView.SelectedRows.Count == 1
+            ? teachersDataGridView.SelectedRows[0].Index
+            : teachersDataGridView.SelectedCells.Count == 1
+            ? teachersDataGridView.SelectedCells[0].RowIndex
+            : (int?)null;
     }
 }
 
