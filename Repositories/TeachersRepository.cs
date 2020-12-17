438f01fd56feb3e12d773f10fade077607de462c
Anton Bodiak <abodiak@prometheusgroup.com>
2020-12-17 08:26:29 +0200

    Move 'TeachersRepository' to a separate namespace

---
+++ Repositories/TeachersRepository.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+
+namespace SPZLab5Var1
+{
+    public static class TeachersRepository
+    {
+        public static List<Teacher> Teachers = new List<Teacher>()
+        {
+            new Teacher { Id = 1, Name = "Иван Иванов", Age = 20 },
+            new Teacher { Id = 2, Name = "Петр Петров", Age = 30 },
+            new Teacher { Id = 3, Name = "Василий Василев", Age = 40 },
+        };
+    }
+}
+
