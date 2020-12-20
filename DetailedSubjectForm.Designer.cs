60cdebfd93bb4e755b615d0d97de3ab25ba0e69f
Anton Bodiak <abodiak@prometheusgroup.com>
2020-12-20 21:14:51 +0200

    Create 'DetailedSubjectForm' and add UI elements to it

---
+++ DetailedSubjectForm.Designer.cs
@@ -0,0 +1,104 @@
+﻿namespace SPZLab5Var1
+{
+    partial class DetailedSubjectForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.nameLabel = new System.Windows.Forms.Label();
+            this.facultyLabel = new System.Windows.Forms.Label();
+            this.saveButton = new System.Windows.Forms.Button();
+            this.facultyTextBox = new System.Windows.Forms.TextBox();
+            this.nameTextBox = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // nameLabel
+            // 
+            this.nameLabel.AutoSize = true;
+            this.nameLabel.Location = new System.Drawing.Point(44, 15);
+            this.nameLabel.Name = "nameLabel";
+            this.nameLabel.Size = new System.Drawing.Size(31, 15);
+            this.nameLabel.TabIndex = 0;
+            this.nameLabel.Text = "Имя";
+            // 
+            // facultyLabel
+            // 
+            this.facultyLabel.AutoSize = true;
+            this.facultyLabel.Location = new System.Drawing.Point(12, 44);
+            this.facultyLabel.Name = "facultyLabel";
+            this.facultyLabel.Size = new System.Drawing.Size(63, 15);
+            this.facultyLabel.TabIndex = 1;
+            this.facultyLabel.Text = "Факультет";
+            // 
+            // saveButton
+            // 
+            this.saveButton.Location = new System.Drawing.Point(81, 70);
+            this.saveButton.Name = "saveButton";
+            this.saveButton.Size = new System.Drawing.Size(75, 23);
+            this.saveButton.TabIndex = 2;
+            this.saveButton.Text = "Сохранить";
+            this.saveButton.UseVisualStyleBackColor = true;
+            // 
+            // facultyTextBox
+            // 
+            this.facultyTextBox.Location = new System.Drawing.Point(81, 41);
+            this.facultyTextBox.Name = "facultyTextBox";
+            this.facultyTextBox.Size = new System.Drawing.Size(100, 23);
+            this.facultyTextBox.TabIndex = 3;
+            // 
+            // nameTextBox
+            // 
+            this.nameTextBox.Location = new System.Drawing.Point(81, 12);
+            this.nameTextBox.Name = "nameTextBox";
+            this.nameTextBox.Size = new System.Drawing.Size(100, 23);
+            this.nameTextBox.TabIndex = 4;
+            // 
+            // DetailedSubjectForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(191, 99);
+            this.Controls.Add(this.nameTextBox);
+            this.Controls.Add(this.facultyTextBox);
+            this.Controls.Add(this.saveButton);
+            this.Controls.Add(this.facultyLabel);
+            this.Controls.Add(this.nameLabel);
+            this.Name = "DetailedSubjectForm";
+            this.Text = "DetailedSubjectForm";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label nameLabel;
+        private System.Windows.Forms.Label facultyLabel;
+        private System.Windows.Forms.Button saveButton;
+        private System.Windows.Forms.TextBox facultyTextBox;
+        private System.Windows.Forms.TextBox nameTextBox;
+    }
+}
---
+++ DetailedSubjectForm.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace SPZLab5Var1
+{
+    public partial class DetailedSubjectForm : Form
+    {
+        public DetailedSubjectForm()
+        {
+            InitializeComponent();
+        }
+    }
+}
